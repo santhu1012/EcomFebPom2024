7fcabe2a953e5e6fa902cf120b4907e3bcc5645b
santhu1012 <santhosh.cs046@gmail.com>
2024-02-20 12:14:06 +0530

    added order page

---
+++ src/main/java/godigit/qa/ecom/pages/OrderPage.java
@@ -0,0 +1,13 @@
+package godigit.qa.ecom.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+
+	By loc = By.id("order");
+	
+	public void getOrder()
+	{
+		System.out.println("get order");
+	}
+}
