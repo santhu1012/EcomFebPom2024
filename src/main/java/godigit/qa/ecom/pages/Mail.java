bf236ae5e218d66604277f8bd6803cd8bf16eb7d
santhu1012 <santhosh.cs046@gmail.com>
2024-02-21 12:15:38 +0530

    mail santhosh

---
+++ src/main/java/godigit/qa/ecom/pages/Mail.java
@@ -0,0 +1,17 @@
+package godigit.qa.ecom.pages;
+
+public class Mail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mail is added -- santhosh");
+
+	}
+	
+	public void mail()
+	{
+		System.out.println("get mail ---santhosh");
+	}
+
+}
